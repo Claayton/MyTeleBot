8ef9d67f1056b20dd91855760fd6b4ebd6482f8b
Clayton Garcia <claaytongarcia@gmail.com>
2022-03-19 17:18:29 -0300

    fix: Atualizado nome de metodo que estava errado

--- mytelebot/interfaces/tele_bot_client_interface.py
+++ mytelebot/interfaces/tele_bot_client_interface.py
@@ -12,7 +12,7 @@ class TeleBotClientInterface(ABC):
         raise Exception("Deve ser implementado o metodo: init_client")
 
     @abstractmethod
-    def new_messages(self):
+    def get_new_messages(self):
         """Deve ser implementado"""
 
         raise Exception("Deve ser implementado o metodo: get_new_messages")
