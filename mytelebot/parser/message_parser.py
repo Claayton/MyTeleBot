7a93972b23f5e4cae7c945366c68c0465db5f090
Clayton Garcia <claaytongarcia@gmail.com>
2022-03-19 18:23:52 -0300

    feat: Implementado o metodo handle_message, para realizar o tratamento da mensagem e auxiliar na filtragem

--- mytelebot/parser/message_parser.py
+++ mytelebot/parser/message_parser.py
@@ -56,37 +56,26 @@ class MessageParser(MessageParserInterface):
 
         return response
 
-    def __is_python_opportunity(
-        self, myself: Type[User], sender: Type[User], event: Type[NewMessage.Event]
-    ):
-        """Verifica se a mensagem é uma oportunidade de vaga python"""
-
-        message = event.message.message.lower().split()
-
-        if sender.username == myself.username:
-
-            return {"success": False, "message": None}
-
-        if "python" not in message:
-
-            return {"success": False, "message": None}
-
-        if self.words_filter(message):
-
-            return {"success": True, "message": event.message.message}
-
-        return {"success": False, "message": None}
-
-    @classmethod
-    def words_filter(cls, message: List):
+    def words_filter(self, message: List):
         """Realiza a filtragem de palavras na mensagem"""
 
-        word_list = ["oportunidade", "vaga", "vagas", "emprego", "trabalho"]
+        handle_message = self.__handle_message(message)
+
+        word_list = [
+            "oportunidade",
+            "oportunidades",
+            "vaga",
+            "vagas",
+            "vaguinha",
+            "vaguinhas",
+            "emprego",
+            "trabalho",
+        ]
         response = False
 
         for word in word_list:
 
-            if word in message:
+            if word in handle_message:
                 response = True
             else:
                 continue
@@ -105,3 +94,39 @@ class MessageParser(MessageParserInterface):
         print(f"\033[34muser_id: \033[35m{sender.id}\033[m")
         print(f"\033[34mSou eu?: \033[35m{sender.is_self}\033[m")
         print(f"\033[34mQuem sou eu?: \033[35m{myself.username}\033[m")
+
+    def __is_python_opportunity(
+        self, myself: Type[User], sender: Type[User], event: Type[NewMessage.Event]
+    ):
+        """Verifica se a mensagem é uma oportunidade de vaga python"""
+
+        message = event.message.message
+        handle_message = self.__handle_message(message)
+
+        if sender.username == myself.username:
+
+            return {"success": False, "message": None}
+
+        if "python" not in handle_message:
+
+            return {"success": False, "message": None}
+
+        if self.words_filter(message):
+
+            return {"success": True, "message": event.message.message}
+
+        return {"success": False, "message": None}
+
+    def __handle_message(self, message: str):
+        """realiza o tratamento da mensagem para auxiliar na filtragem"""
+
+        handle_message = (
+            message.lower()
+            .replace(",", "")
+            .replace(".", "")
+            .replace("!", "")
+            .replace("?", "")
+            .replace("-", "")
+        )
+
+        return handle_message.split()
