52a10521ae1dbb6ff7a365499d371153c3f66119
Clayton Garcia <claaytongarcia@gmail.com>
2022-04-16 20:11:53 -0300

    test✅: Atualizados os testes para message_parser + Implementado fixtures

--- tests/parser_test/message_parser_test.py
+++ tests/parser_test/message_parser_test.py
@@ -1,21 +1,20 @@
 """Testes para a classe MessageParser"""
+from pytest import mark
 from faker import Faker
 from telethon.tl.types import User, Message, PeerUser
 from telethon.events import NewMessage
-from mytelebot.parser import MessageParser
 from mytelebot.config import API
 
+
 fake = Faker()
-parser = MessageParser()
 
 
-def test_parse_message_where_the_message_is_from_the_user():
+def test_parse_message_where_the_message_is_from_the_user(parser, myself):
     """
     Testando o metodo parse_message, onde a mensagem vem do proprio usuario.
     O sistema deve retornar None.
     """
 
-    myself = User(id=fake.random_number(), username=fake.name())
     sender = User(id=fake.random_number(), username=myself.username)
     event = NewMessage.Event(
         Message(
@@ -28,18 +27,17 @@ def test_parse_message_where_the_message_is_from_the_user():
 
     response = parser.parse_message(myself, sender, event)
 
-    assert response is None
+    assert response["success"] is False
+    assert response["message"] is None
 
 
-def test_parse_with_oi_message():
+def test_parse_with_oi_message(parser, myself, sender):
     """
     Testando o metodo parse_message, com mensagem de oi.
     Nesse caso a mensagem deve passar e retornar os dados necessarios,
     Para responder a mensagem ao mesmo usuario que enviou.
     """
 
-    myself = User(id=fake.random_number(), username=fake.name())
-    sender = User(id=fake.random_number(), username=fake.name())
     event = NewMessage.Event(
         Message(
             id=fake.random_number(),
@@ -56,15 +54,13 @@ def test_parse_with_oi_message():
     assert response["message"] == "Oi, sou um bot de testes!"
 
 
-def test_parse_with_noia_message():
+def test_parse_with_noia_message(parser, myself, sender):
     """
     Testando o metodo parse_message, com mensagem de noia.
     Nesse caso a mensagem deve passar e retornar os dados necessarios,
     Para responder a mensagem ao mesmo usuario que enviou.
     """
 
-    myself = User(id=fake.random_number(), username=fake.name())
-    sender = User(id=fake.random_number(), username=fake.name())
     event = NewMessage.Event(
         Message(
             id=fake.random_number(),
@@ -81,7 +77,7 @@ def test_parse_with_noia_message():
     assert response["message"] == "Aloprado! (mensagem automatica)"
 
 
-def test_parse_with_python_message_and_positive_filter():
+def test_parse_with_python_message_and_positive_filter(parser, myself, sender):
     """
     Testando o metodo parse_message, com python na mensagem e o filtro positivo.
     Recebendo uma mensagem com uma palavra do filtro.
@@ -89,8 +85,6 @@ def test_parse_with_python_message_and_positive_filter():
     Para o envio da mensagem ao grupo.
     """
 
-    myself = User(id=fake.random_number(), username=fake.name())
-    sender = User(id=fake.random_number(), username=fake.name())
     event = NewMessage.Event(
         Message(
             id=fake.random_number(),
@@ -107,15 +101,13 @@ def test_parse_with_python_message_and_positive_filter():
     assert response["message"] == event.message.message
 
 
-def test_parse_with_python_message_and_negative_filter():
+def test_parse_with_python_message_and_negative_filter(parser, myself, sender):
     """
     Testando o metodo parse_message, com python na mensagem e o filtro negativo.
     Recebendo uma mensagem sem nenhuma das palavras do filtro.
     Nesse caso a mensagem nao deve passar e retornar None.
     """
 
-    myself = User(id=fake.random_number(), username=fake.name())
-    sender = User(id=fake.random_number(), username=fake.name())
     event = NewMessage.Event(
         Message(
             id=fake.random_number(),
@@ -130,14 +122,12 @@ def test_parse_with_python_message_and_negative_filter():
     assert response is None
 
 
-def test_parse_without_any_by_the_filter_parameters():
+def test_parse_without_any_by_the_filter_parameters(parser, myself, sender):
     """
     Testando o metodo parse_message, onde a mensagem nao contem nenhum dos parametros do filtro.
     O sistema deve retornar None.
     """
 
-    myself = User(id=fake.random_number(), username=fake.name())
-    sender = User(id=fake.random_number(), username=myself.username)
     event = NewMessage.Event(
         Message(
             id=fake.random_number(),
@@ -152,53 +142,45 @@ def test_parse_without_any_by_the_filter_parameters():
     assert response is None
 
 
-def test_words_filter_with_positive_filter():
+@mark.parametrize(
+    "message",
+    [
+        ("Oportunidade de python saindo do forno guys!"),
+        ("Tenho varias oportunidades para dev python rapaziada"),
+        ("Tem vaga nova aqui na firma"),
+        ("Tem muitas vagas novas aqui na firma"),
+        ("Piazada, tem uma vaguinha de python muito boa aqui, se interessa?"),
+        ("Vaguinhas fresquinhas aqui guys"),
+        ("Quem estiver procurando emprego, eu estou com um de python aqui ein"),
+        ("tem trabalho para programador python aqui"),
+    ],
+)
+def test_words_filter_with_positive_filter(parser, message):
     """
     Testando o metodo words_filter.
     Realizao o teste com frases contendo todas as palavras do filtro para garantir que todas passam.
     Nesse caso deve retornar True.
     """
 
-    message1 = "Oportunidade de python saindo do forno guys!"
-    message2 = "Tenho varias oportunidades para dev python rapaziada"
-    message3 = "Tem vaga nova aqui na firma"
-    message4 = "Tem muitas vagas novas aqui na firma"
-    message5 = "Piazada, tem uma vaguinha de python muito boa aqui, se interessa?"
-    message6 = "Vaguinhas fresquinhas aqui guys"
-    message7 = "Quem estiver procurando emprego, eu estou com um de python aqui ein"
-    message8 = "tem trabalho para programador python aqui"
+    response = parser.words_filter(message)
 
-    response1 = parser.words_filter(message1)
-    response2 = parser.words_filter(message2)
-    response3 = parser.words_filter(message3)
-    response4 = parser.words_filter(message4)
-    response5 = parser.words_filter(message5)
-    response6 = parser.words_filter(message6)
-    response7 = parser.words_filter(message7)
-    response8 = parser.words_filter(message8)
-
-    assert response1 is True
-    assert response2 is True
-    assert response3 is True
-    assert response4 is True
-    assert response5 is True
-    assert response6 is True
-    assert response7 is True
-    assert response8 is True
+    assert response is True
 
 
-def test_words_filter_with_negative_filter():
+@mark.parametrize(
+    "message",
+    [
+        ("Bom dia piazada o python é uma linguagem muito boa"),
+        ("estou estudando Python, que bom para mim"),
+    ],
+)
+def test_words_filter_with_negative_filter(parser, message):
     """
     Testando o metodo words_filter.
     Realizao o teste com frases que nao contem nenhum palavra do filtro.
     Nesse caso deve retornar False.
     """
 
-    message1 = "Bom dia piazada o python é uma linguagem muito boa"
-    message2 = "estou estudando Python, que bom para mim"
+    response = parser.words_filter(message)
 
-    response1 = parser.words_filter(message1)
-    response2 = parser.words_filter(message2)
-
-    assert response1 is False
-    assert response2 is False
+    assert response is False
